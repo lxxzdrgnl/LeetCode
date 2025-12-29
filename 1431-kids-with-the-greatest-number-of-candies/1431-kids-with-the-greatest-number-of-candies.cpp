acdb30da462493fc90457795f8e56a4704189e65
Lee YongJae <165778303+lxxzdrgnl@users.noreply.github.com>
2025-12-29 13:41:39 +0900

    Time: 0 ms (100%), Space: 12.5 MB (58.01%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+            int n = candies.size();
+
+            int maxCandies = 0;
+            for (int i = 0; i < n; i ++){
+                if (candies[i] > maxCandies) maxCandies = candies[i];
+            }
+
+            vector<bool> result(n);
+            for (int i = 0; i < n; i++) {
+                result[i] = (candies[i] + extraCandies >= maxCandies);
+        }
+        return result;
+    }
+};
