f2033d77c84681ede44d43df3fe93af393e12ae3
Lee YongJae <165778303+lxxzdrgnl@users.noreply.github.com>
2026-02-10 15:52:58 +0900

    Time: 0 ms (100%), Space: 11.5 MB (67.14%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n = senate.size();
+        queue<int> radiant, dire;
+
+        for (int i = 0; i < n; i++) {
+            if (senate[i] == 'R')
+                radiant.push(i);
+            else
+                dire.push(i);
+        }
+
+        while (true) {
+            if (radiant.empty()) return "Dire";
+            if (dire.empty()) return "Radiant";
+
+            int r_idx = radiant.front();
+            radiant.pop();
+            int d_idx = dire.front();
+            dire.pop();
+
+            if (r_idx < d_idx) {
+                radiant.push(r_idx + n);
+            } else {
+                dire.push(d_idx + n);
+            }
+        }
+    }
+};
